83bb7d91ebabed0c46e61b1642e532ffe013b8d2
LuizzDutra <luizzidutra@gmail.com>
2022-09-17 12:54:48 -0300

    uso_de_resources_na_customização

--- src/Scripts/Global.gd
+++ src/Scripts/Global.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 onready var game_scene = load("res://Scenes/Game.tscn")
 onready var menu = $Menu
-onready var player_data = $player_data
+onready var player_data = load("res://resources/data_cust.tres")
 var sprite_color_parts = ["Body", "Hair", "Eyes"]
 var sprite_frame_parts = ["Body", "Hair", "Eyes", "Pants", "Shirt"]
 
@@ -11,24 +11,10 @@ func _ready():
 
 func start_game():
 	var game = game_scene.instance()
-	#carrega a informação da cor do Player
-	for i in range(len(sprite_color_parts)):
-		var body_part = game.get_node("Player/Sprites/" + sprite_color_parts[i])
-		var color_part = player_data.data[sprite_color_parts[i] + "_color"]
-		#checka se o shader tem modulate próprio
-		if body_part.get_material() != null:
-			if body_part.get_material().get_shader_param("modulate") != null:
-				body_part.get_material().set_shader_param("modulate", color_part)
-			else:
-				body_part.modulate = color_part
-		else:
-			body_part.modulate = color_part
-	
 	game.connect("return_to_menu", self, "_on_return_to_menu")
 	add_child(game)
 
 func _on_info_saved(s_var, info):
-	print(info)
 	if player_data.data.has(s_var):
 		player_data.data[s_var] = info
 
--- src/Scripts/PlayerCust.gd
+++
@@ -1,6 +0,0 @@
-extends "res://Scripts/player_data.gd"
-#carrega a configuração default do player_data.gd
-func _ready():
-	$Hair.modulate = data["Hair_color"]
-	$Body.modulate = data["Body_color"]
-	#$Eyes.modulate = data["Eyes_color"]
