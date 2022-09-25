5ad25d2000d99d0bd3888c41a057dbe76f34ea3e
Lucaswpp <lucascardosk27@gmail.com>
2022-09-25 11:43:17 -0300

    sistema de quest/árvore de dialogo

--- src/Scripts/unity.gd
+++ src/Scripts/unity.gd
@@ -6,16 +6,26 @@ var dialog_npc_spc_base = [
 	{"name":"Luiza", "text":"Você poderia achar meus óculos perdidos por favor ?"}
 ]
 
+var dialog_npc_spc_missao_aceita = [
+	{"name":"Luiza","text":"Eu não sei o que eu vou fazer da minha vida se eu não achar esse óculos"},
+	{"name":"Luiza","text":"Esse óculos é muito importante para mim"},
+	{"name":"Luiza","text":"..."}
+]
 var dialog_npc_spc_missao_concluida = [
 	{"name":"Luiza","text":"Muito obrigada garoto."},
 	{"name":"Luiza","text":"Agora eu vou voltar para a sala de aula."},
 	{"name":"Luiza","text":"Tenha uma boa aula."}
 ]
 
-var dialog_npc_esp
+var dialog_state = 1
+onready var quest = get_node("../../Quest")
+var quest_descricao = "Pegar o óculos da sua amiga"
+
 
 func _ready():
-	dialog_npc_esp = dialog_npc_spc_base
+	var nodepath: NodePath = "../../Quest"
+	print(get_node(nodepath).get_children())
+	
 
 onready var hitbox_npc_spc = $Area2D
 
@@ -26,8 +36,23 @@ func _input(event):
 func find_and_use_dialogue():
 	var dialogue_player = get_node_or_null("Area2D/dialogo")
 	
-	if dialogue_player:
-			dialogue_player.play_dialog(dialog_npc_esp)
+	match dialog_state:
+		
+		1:
+			dialogue_player.play_dialog(dialog_npc_spc_base)
+			if dialogue_player.current_index >= len(dialog_npc_spc_base):
+				quest.add_quest(quest_descricao)
+				dialog_state = 2
+			return
+		
+		2:
+			dialogue_player.play_dialog(dialog_npc_spc_missao_aceita)
+			return
+		3:
+			dialogue_player.play_dialog(dialog_npc_spc_missao_concluida)
+			if dialogue_player.current_index >= len(dialog_npc_spc_missao_concluida):
+				quest.kill_quest(quest_descricao)
+			return
 		
 func _on_oculos_oculos_catch():
-	dialog_npc_esp = dialog_npc_spc_missao_concluida
+	dialog_state = 3
