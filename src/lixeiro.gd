78236b7c45dcef21839f4b5a6d86d704af1659e2
Lucaswpp <lucascardosk27@gmail.com>
2022-11-06 18:13:00 -0300

    task do zelador concluida

--- src/lixeiro.gd
+++ src/lixeiro.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var hitbox_lixeiro = $Area2D
 onready var player = get_node("../YSort/Player")
+onready var task_zelador = get_node("../zelador/task_zelador")
 var cont_lixeiro
 
 func _ready():
@@ -9,6 +10,10 @@ func _ready():
 
 func _input(event):
 	
-	if event.is_action_pressed("enter") and len(hitbox_lixeiro.get_overlapping_areas()) > 0:
+	if event.is_action_pressed("enter") and len(hitbox_lixeiro.get_overlapping_areas()) > 0 and task_zelador.zelado.dialog_state == 1:
+		
+		print("aaaaa")
 		cont_lixeiro = player.cont_lixo
-		print(cont_lixeiro)
+		
+		if cont_lixeiro >= 5:
+			task_zelador.zelado.dialog_state = 2
--- src/zelador.gd
+++ src/zelador.gd
@@ -1,6 +1,9 @@
 extends StaticBody2D
 
 onready var hitbox_zelador = $Area2D
+onready var zelador_task = $task_zelador
+
+var dialog_state
 
 var zelador_dialogo_base = [
 	{"name":"Zelador","text":"eae meu filho como ocê tá ?"},
@@ -17,8 +20,9 @@ var zelado_dialogo_missao_concluida = [
 	{"name":"Zelador","text":"Parábens ocê é muito rapido!!!"},
 	{"name":"zelador","text":"Parábens ocê é o filho do flash kkkk"}
 ]
+
 func _ready():
-	pass
+	dialog_state = 0
 
 func _input(event):
 	if event.is_action_pressed("space") and len(hitbox_zelador.get_overlapping_areas()) > 0:
@@ -26,4 +30,20 @@ func _input(event):
 		get_tree().set_input_as_handled()
 
 func find_and_use_dialogue():
-	pass
+	
+	var dialog_npc = get_node("dialogo")
+	
+	match dialog_state:
+		
+		0:
+			dialog_npc.play_dialog(zelador_dialogo_base)
+		1:
+			dialog_npc.play_dialog(zelador_dialogo_start)
+		2:
+			dialog_npc.play_dialog(zelado_dialogo_missao_concluida)
+
+
+func _on_dialogo_ended():
+	if dialog_state == 0:
+		dialog_state = 1
+		zelador_task.iniciar_task()
